e6289ef55778818584a187ef1d3e22914e06ffa1
KaikSAlves <souzaalveskaik@gmail.com>
2026-02-11 13:35:39 -0300

    if e else no jsx

--- src/components/ConditionalRender.js
+++ src/components/ConditionalRender.js
@@ -4,11 +4,38 @@ const ConditionalRender = () => {
 
   const [x] = useState(true);
 
+  const [name, setName] = useState("Carlos");
+
+  const transformName = () => {
+    
+    setName((prevState) => {
+
+        if (prevState == "João") return "Carlos";
+
+        
+        return "João";
+    });
+  }
+
+
   return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true, sim!</p>}
         {!x && <p>Agora x é falso!</p>}
+
+        <h1>If ternário</h1>
+        <button onClick={() => transformName()}>Clique aqui para transformar seu nome!</button>
+        {name == "João"? (
+            <div>
+                <p>O nome é {name}</p>
+            </div>
+        ) : 
+        (
+            <div>
+                <p>O nome não é João, e sim {name}</p>
+            </div>
+        )}
     </div>
   )
 }   
