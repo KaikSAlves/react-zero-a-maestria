75d25191aca38cc144da597a91b929482f2bfb9f
KaikAlves <souzaalveskaik@gmail.com>
2026-02-13 20:30:02 -0300

    Css de componentes e Css global

---
+++ CSS_REACT/css/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyComponent from './components/MyComponent/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent></MyComponent>
+      <p>Vazou a cor?</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ CSS_REACT/css/src/components/MyComponent/MyComponent.js
@@ -0,0 +1,15 @@
+import "./MyComponent.css";
+
+const MyComponent  = () => {
+    return (
+        <div>
+            <h1>Meu Componente</h1>
+            <p>Este é o parágrafo do meu componente</p>
+            <p className="my-component-text">Este também é do componente, onde se utilizamos o className para 
+                estilização de componentes, evitamos que ele vaze para outras tags
+            </p>
+        </div>
+    );
+}
+
+export default MyComponent;
