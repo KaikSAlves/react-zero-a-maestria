c4bf2783a383025d23ce2ecfda6bab8bb6e52109
KaikAlves <souzaalveskaik@gmail.com>
2026-02-13 20:39:26 -0300

    css inline dinamico

--- CSS_REACT/css/src/components/MyComponent/MyComponent.js
+++ CSS_REACT/css/src/components/MyComponent/MyComponent.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import "./MyComponent.css";
 
 const MyComponent  = () => {
+
+    const [color, setColor] = useState("blue");
+
+    const changeColor = (color) => {
+        setColor(color)
+    }
+
     return (
         <div>
             <h1>Meu Componente</h1>
@@ -9,6 +17,11 @@ const MyComponent  = () => {
                 estilização de componentes, evitamos que ele vaze para outras tags
             </p>
             <p style={{color: "blue"}}>Css inline do React</p>
+
+            <p style={ color == "blue"? {color: "blue"} : color == "red"? {color: "red"} : {}}>css inline dinamico</p>
+            <p style={ color == "blue"? ({color: "red"}) : color == "red"? ({color: "blue"}) : {}}>invertendo as coisas kk</p>
+            <button onClick={() => changeColor("blue")}>Deixa inline dinamico AZUL!</button>
+            <button onClick={() => changeColor("red")}>Deixa inline dinamico VERMELHO!</button>
         </div>
     );
 }
